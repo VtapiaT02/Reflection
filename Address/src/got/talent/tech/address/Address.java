23e90ce1fac8b2eee50baf738c2adb0f6bbd63f6
Victor Tapia <vtapia02@hotmail.com>
2024-06-08 08:07:07 -0600

    subiendo proyecto

---
+++ Address/src/got/talent/tech/address/Address.java
@@ -0,0 +1,79 @@
+package gto.talent.tech.address;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+    private static Integer id = 0;
+
+    public Address(String street, String city, String state, String zip, String country) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void showMessage() {
+        System.out.println("Welcome to " + city + ", " + state + "!");
+    }
+
+    public static Integer setId() {
+        return id + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
---
+++ Modules/src/got/talent/tech/Main.java
@@ -0,0 +1,7 @@
+package got.talent.tech;
+
+public class Main {
+    public static void main(String[] args) {
+        Address add = new Address();
+    }
+}
